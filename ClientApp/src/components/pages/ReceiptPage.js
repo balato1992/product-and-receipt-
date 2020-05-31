11facf335cc41035574b4fe5fdf34736adae6475
chadhong <balato1992@gmail.com>
2020-05-31 10:55:18 +0800

    remove useless component

--- ClientApp/src/components/pages/ReceiptPage.js
+++ ClientApp/src/components/pages/ReceiptPage.js
@@ -1,128 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import { CusTable } from '../items/CusTable';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-
-import AddBoxIcon from '@material-ui/icons/AddBox';
-
-import { ReceiptRow, SelectedRowMode } from '../items/ReceiptRow';
 import * as Methods from '../../Methods'
 
 export function ReceiptPage() {
-
-
-    const [receipts, setReceipts] = useState([]);
-    const [selectedRowAndMode, setSelectedRowAndMode] = useState(undefined);
-
-    const [addRowItem, setAddRowItem] = useState(getNewReceipt());
-    const [showAddItem, setShowAddItem] = useState(false);
-
     const baseUrl = "ReceiptInfo";
-
-    useEffect(() => {
-        getData();
-    }, []);
-
-    function getData() {
-
-        fetch(baseUrl)
-            .then(response => response.json())
-            .then(result => {
-
-                for (let datum of result) {
-
-                    datum.uidForView = Methods.cusGetUidForView();
-
-                    for (let item of datum.items) {
-                        item.uidForView = Methods.cusGetUidForView();
-                    }
-                }
-                setReceipts(result);
-            });
-    };
-    function getDataAction() {
-        return (data, doneFunc) => {
-
-            if (selectedRowAndMode === undefined) {
-                alert("發生錯誤 0012");
-                return;
-            }
-
-            let method = "";
-            switch (selectedRowAndMode.mode) {
-                case SelectedRowMode.AddMode:
-                    method = 'post';
-                    break;
-                case SelectedRowMode.ModifyMode:
-                    method = 'patch';
-                    break;
-                case SelectedRowMode.DeleteMode:
-                    method = 'delete';
-                    data = data.uid;
-                    break;
-                default:
-                    alert("發生錯誤 0012");
-                    return;
-            }
-
-            Methods.cusFetch(baseUrl, method, data,
-                () => {
-                    //alert("上傳成功");
-
-                    doneFunc();
-                    getData();
-                },
-                () => {
-                    alert("上傳失敗");
-                });
-        };
-    }
-
-    function getNewReceipt() {
-        let date = new Date();
-        date.setHours(date.getHours() + 8);
-
-        return {
-            id: "-",
-            payee: "-",
-            date: date.toISOString().substr(0, 19),
-            items: []
-        };
-    }
-
-    function addClick() {
-
-        if (!showAddItem) {
-            let receipt = getNewReceipt();
-            setAddRowItem(receipt);
-            onSelectedRow(receipt, SelectedRowMode.AddMode);
-        }
-
-        setShowAddItem(!showAddItem);
-    }
-    function onSelectedRow(rowData, mode) {
-
-        if (rowData !== undefined
-            && mode !== undefined
-            && (selectedRowAndMode === undefined || selectedRowAndMode.rowData !== rowData)
-        ) {
-            setSelectedRowAndMode({
-                rowData: rowData,
-                mode: mode
-            });
-        } else {
-            setSelectedRowAndMode(undefined);
-        }
-    }
-
-
 
 /*
     public class ReceiptDatumWithUid
@@ -189,42 +71,6 @@ export function ReceiptPage() {
     return (
         <div>
             <CusTable columns={columns} getDataCallback={getDataCallback} editActions={editActions} usingReceiptDetail={true}></CusTable>
-            <br />
-            <br />
-            <br />
-            <br />
-            <Button variant="outlined" startIcon={<AddBoxIcon />} style={{ margin: "8px" }} onClick={addClick} >新增收據紀錄</Button>
-
-            <div style={{ maxWidth: '100%' }}>
-                <TableContainer component={Paper}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell style={{ minWidth: "200px" }}></TableCell>
-                                <TableCell>編號</TableCell>
-                                <TableCell>客戶名稱</TableCell>
-                                <TableCell style={{ minWidth: "100px" }}>日期</TableCell>
-                                <TableCell></TableCell>
-                                <TableCell></TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {showAddItem &&
-                                <ReceiptRow selectedRowAndMode={selectedRowAndMode} inputData={addRowItem}
-                                    confirmAction={getDataAction()}
-                                    onActionDone={() => { setShowAddItem(false); }}
-                                    onSelectedRow={onSelectedRow} ></ReceiptRow>
-                            }
-                            {receipts.map((item, index) => (
-                                <ReceiptRow
-                                    key={item.uid} selectedRowAndMode={selectedRowAndMode} inputData={item}
-                                    confirmAction={getDataAction()}
-                                    onSelectedRow={onSelectedRow} ></ReceiptRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </div>
         </div>
     );
 }
